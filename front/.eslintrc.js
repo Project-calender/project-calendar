20e7a6a34df8db935eccd659cc6a634453441947
정진아 <human00024@naver.com>
2022-06-23 16:45:55 +0900

    chore: React + ESLint + Prettier 설정

---
+++ front/.eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react'],
+  rules: {
+    'prettier/prettier': ['warn'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  },
+};
