6c761518f016101d12c22b170c06403037e41a14
JINA <52408122+pro-popo@users.noreply.github.com>
2022-06-27 15:57:07 +0900

    chore: ESLint 설정 파일 수정 (#19) (#20)

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -4,22 +4,26 @@ module.exports = {
     es2021: true,
     node: true,
   },
+
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:prettier/recommended',
   ],
+
+  plugins: ['react'],
+
+  rules: {
+    'prettier/prettier': ['warn'],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
+  },
+
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 'latest',
     sourceType: 'module',
-  },
-  plugins: ['react'],
-  rules: {
-    'prettier/prettier': ['warn'],
-    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
   },
 };
