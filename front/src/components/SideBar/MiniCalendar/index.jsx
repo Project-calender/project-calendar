a21fc91227833e93743aed0b4e090bc7c42f9ac7
정진아 <human00024@naver.com>
2022-06-29 05:10:23 +0900

    feat: SideBar 컴포넌트 구현 - 사용자 검색, SideBar 컴포넌트 구성 (#28)

---
+++ front/src/components/SideBar/MiniCalendar/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>미니캘린더</div>;
+};
+
+export default index;
--- front/src/components/SideBar/index.jsx
+++ front/src/components/SideBar/index.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import styles from './style.module.css';
 import AddEventButton from './AddEventButton';
+import UserSearch from './UserSearch';
+import MyCalendarList from './calendars/MyCalendarList';
+import SubscriptionCalendarList from './calendars/SubscriptionCalendarList';
 
-const index = () => {
+const Index = () => {
   return (
-    <aside className={styles.container}>
-      <AddEventButton />
+    <aside className={styles.sidebar}>
+      <div className={styles.sidebar_event_button}>
+        <AddEventButton />
+      </div>
+      <div className={styles.sidebar_calender}>
+        <UserSearch />
+        <MyCalendarList />
+        <SubscriptionCalendarList />
+      </div>
     </aside>
   );
 };
 
-export default index;
+export default Index;
