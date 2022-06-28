0047692d991cc2babc3c3636bddb99f8bb2784e7
이민서 <sola2014@naver.com>
2022-06-28 15:41:47 +0900

    feat: makemigartions - athority-column-CalendarMembers

--- back/routes/user.js
+++ back/routes/user.js
@@ -1,153 +1,202 @@
-const express = require("express");
-const bcrypt = require("bcrypt");
-const passport = require("passport");
+const express = require('express');
+const bcrypt = require('bcrypt');
+const passport = require('passport');
 const jwt = require("jsonwebtoken");
+const { Op } = require("sequelize");
 
-const { User, PrivateCalendar } = require("../models");
-const router = express.Router();
+const { User } = require('../models');
+const { PrivateCalendar } = require('../models')
+const { PrivateEvent } = require('../models')
+const { reset } = require('nodemon');
+const router = express.Router()
 
 // const { verifyToken } = require('./memaildleware');
 
-router.post("/login", (req, res, next) => {
-  passport.authenticate("signin", (err, user, info) => {
-    try {
-      console.log("??");
-      if (err) {
-        console.error(err);
-        next(err);
-      }
-      if (info) {
-        return res.status(401).send(info);
-      }
-      return req.login(user, { session: false }, async (loginErr) => {
-        if (loginErr) {
-          return next(loginErr);
-        }
-        const refreshToken = jwt.sign(
-          {
-            sub: "refresh",
-            email: user.email,
-          },
-          "jwt-secret-key",
-          { expiresIn: "24h" }
-        );
-        const accessToken = jwt.sign(
-          {
-            sub: "access",
-            email: user.email,
-          },
-          "jwt-secret-key",
-          {
-            expiresIn: "5m",
-          }
-        );
-        const fullUserWithoutPassword = await User.findOne({
-          where: { email: user.email },
-          attributes: {
-            exclude: ["password"],
-          },
-        });
-        console.log("로그인 성공 확인");
-        return res.status(200).send({
-          fullUserWithoutPassword,
-          refreshToken,
-          accessToken,
-        });
-      });
-    } catch (error) {
-      console.log(error);
-      next(error);
-    }
-  })(req, res, next);
-});
 
-router.post("/signup", async (req, res, next) => {
-  try {
+// 개인 일정 가져오기(년)
+router.get('/year/:year/:month/:day', async (req, res, next) => {
     const exUser = await User.findOne({
-      where: {
-        email: req.body.email,
-      },
-    });
-    if (exUser) {
-      return res.status(403).send("이미 사용중인 아이디입니다.");
+        where: {
+            id: 1
+        }
+    })
+    const startdate = req.params.year
+    const enddate = String(Number(req.params.year)+1)
+    const exEvent = await exUser.getMyEvent({
+        attributes: ['StartTime'],
+        where: {
+            startTime: {
+                [Op.between]: [startdate, enddate]
+        }
     }
-    const hashedPassword = await bcrypt.hash(req.body.password, 12);
-    const newUser = await User.create({
-      email: req.body.email,
-      password: hashedPassword,
-      nickname: req.body.nickname,
-    });
+})  
 
-    const myCalendar = await PrivateCalendar.create({
-      name: "MyCalendar",
-    });
-    await newUser.addMyCalendar(myCalendar);
-    res.status(201).send("ok");
-    // res.status(201).send('회원가입이 완료되었습니다.')
-    console.log("회원가입 확인");
-  } catch (error) {
-    console.log(error);
-    next(error);
-  }
+
+    return res.json({exEvent : exEvent})
+})
+
+
+// 개인 일정 가져오기(월)
+router.get('/month/:year/:month/:day', async (req, res, next) => {
+    const exUser = await User.findOne({
+        where: {
+            id: 1
+        }
+    })
+    const startdate = req.params.year+'-'+req.params.month
+    const enddate = req.params.year+'-'+String(Number(req.params.month)+1)
+    const exEvent = await exUser.getMyEvent({
+        attributes: ['StartTime'],
+        where: {
+            startTime: {
+                [Op.between]: [startdate, enddate]
+            }
+        }
+    })  
+    return res.json({exEvent : exEvent})
+})
+
+router.post('/login', (req, res, next) => {
+    passport.authenticate('signin' ,(err, user, info) => {
+        try{
+            if (err) {
+                console.error(err);
+                next(err);
+            }
+            if(info) {
+                return res.status(401).send(info);
+            }
+            return req.login(user, { session: false },async (loginErr) => {
+                if (loginErr) {
+                    return next(loginErr);
+                }
+                const refreshToken = jwt.sign(
+                    {
+                        sub: "refresh",
+                        email: user.email,
+                    },
+                    "jwt-secret-key",
+                    { expiresIn: "24h" }
+                );
+                const accessToken = jwt.sign(
+                    {
+                        sub: "access",
+                        email: user.email
+                    },
+                    "jwt-secret-key",
+                    {
+                        expiresIn: "5m",
+                    }
+                );
+                const fullUserWithoutPassword = await User.findOne({
+                    where: { email: user.email },
+                    attributes: {
+                        exclude: ['password']
+                    }
+                })
+                console.log("로그인 성공 확인")
+                return res.status(200).send({
+                    fullUserWithoutPassword,
+                    refreshToken,
+                    accessToken,
+                });
+            })
+        } catch (error) {
+            console.log(error);
+            next(error);
+        }
+    })(req, res, next);
 });
+
+router.post('/signup', async (req, res, next) => {
+    try{
+        const exUser = await User.findOne({
+            where: {
+                email: req.body.email,
+            }
+        });
+        if (exUser) {
+            return res.status(403).send('이미 사용중인 아이디입니다.')
+        }   
+        const hashedPassword = await bcrypt.hash(req.body.password, 12)
+        const newUser = await User.create({
+            email: req.body.email,
+            password: hashedPassword,
+            nickname: req.body.nickname
+        })
+        
+        const myCalendar = await PrivateCalendar.create({
+            name: 'MyCalendar'
+        })
+        
+        await newUser.addMyCalendar(myCalendar)
+
+        // await newUser.addMyCalendar({
+        //     name: 'MyCalendar'
+        // })
+    
+        // console.log(User.getOwnPropertyNames(),"dasd")
+        res.status(201).send('ok');
+        // res.status(201).send('회원가입이 완료되었습니다.')
+        console.log("회원가입 확인")
+    } catch (error) {
+        console.log(error);
+        next(error)
+    }
+})
 
 router.post("/refreshToken", async (req, res, next) => {
-  passport.authenticate(
-    ("jwt",
-    { session: false },
-    (err, user, info) => {
-      try {
-        if (err) {
-          console.error(err);
-          return next(err);
+    passport.authenticate(("jwt", { session: false }, (err, user, info) => {
+        try {
+            if(err) {
+                console.error(err);
+                return next(err);
+            }
+            if(info) {
+                if (info?.name === "TokenExpiredError") {
+                //refresh토큰 마저 만료
+                    return res.status(419).send({ error: info.name });
+                }
+                if (info?.name === "JsonWebTokenError") {
+                //refresh토큰 잘못됨
+                    return res.status(419).send({ error: info.name });
+                }
+            }
+            //토큰 재발급
+            const refreshToken = jwt.sign(
+                {
+                    sub: "refresh",
+                    email: user.email,
+                },
+                "jwt-secret-key",
+                { expiresIn: "24h" }
+            );
+            const accessToken = jwt.sign(
+                { 
+                    sub: "access",
+                    email: user.email
+                },
+                "jwt-secret-key",
+                {
+                    expiresIn: "5m",
+                }
+            );
+            return res.status(200).send({
+                email: email,
+                refreshToken,
+                accessToken,
+            })
+        } catch(error) {
+            console.error(error);
+            next(error);
         }
-        if (info) {
-          if (info?.name === "TokenExpiredError") {
-            //refresh토큰 마저 만료
-            return res.status(419).send({ error: info.name });
-          }
-          if (info?.name === "JsonWebTokenError") {
-            //refresh토큰 잘못됨
-            return res.status(419).send({ error: info.name });
-          }
-        }
-        //토큰 재발급
-        const refreshToken = jwt.sign(
-          {
-            sub: "refresh",
-            email: user.email,
-          },
-          "jwt-secret-key",
-          { expiresIn: "24h" }
-        );
-        const accessToken = jwt.sign(
-          {
-            sub: "access",
-            email: user.email,
-          },
-          "jwt-secret-key",
-          {
-            expiresIn: "5m",
-          }
-        );
-        return res.status(200).send({
-          email: email,
-          name: user.name,
-          refreshToken,
-          accessToken,
-        });
-      } catch (error) {
-        console.error(error);
-        next(error);
-      }
-    })
-  );
+    }))
+})
+
+router.post('/user/logout', (req, res) => {
+    req.logout();
+    res.send('ok');
 });
 
-router.post("/user/logout", (req, res) => {
-  req.logout();
-  req.session.destroy();
-  res.send("ok");
-});
 
-module.exports = router;
+module.exports = router
