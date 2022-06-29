dc1aa65db853dcc4ce5e2fe8782c5f6935eae589
hoseol OH <dhgh9590@naver.com>
2022-06-29 17:14:10 +0900

    NavBar 구현및 router 설정(#31)

--- front/src/index.js
+++ front/src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/calendar">
         <App />
       </BrowserRouter>
     </Provider>
---
+++ front/src/pages/Main/index.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Outlet } from 'react-router-dom';
+import styles from './style.module.css';
+import NavBar from '../../components/Navbar';
+
+const Index = () => {
+  /* NavBar */
+  let [activeClass, setActiveClass] = useState(); //일정 리스트에 클레스 추가
+  let [dateActive, setDateActive] = useState(false); //일정 리스트 true ,false 확인
+  let [userClassAdd, setUserClassAdd] = useState(); //일정 리스트에 클레스 추가
+  let [userActive, setUserActive] = useState(false); //일정 리스트 true ,false 확인
+  //body클릭시 일정 리스트 모달창 닫기
+  document.body.addEventListener('click', () => {
+    setDateActive(false);
+    setActiveClass(``);
+    setUserActive(false);
+    setUserClassAdd(``);
+  });
+
+  return (
+    <div>
+      <section className={styles.section}>
+        <NavBar
+          activeClass={activeClass}
+          setActiveClass={setActiveClass}
+          dateActive={dateActive}
+          setDateActive={setDateActive}
+          userClassAdd={userClassAdd}
+          setUserClassAdd={setUserClassAdd}
+          userActive={userActive}
+          setUserActive={setUserActive}
+        ></NavBar>
+        <Outlet></Outlet>
+      </section>
+    </div>
+  );
+};
+
+export default Index;
