f2e1984d56a92863b49625064a54b256b956188a
정진아 <human00024@naver.com>
2022-06-29 15:55:34 +0900

    feat: MiniCalendar 컴포넌트 - moment 모듈 기능 추가 (#30)

--- front/src/utils/moment.js
+++ front/src/utils/moment.js
@@ -1,33 +1,47 @@
 export const WEEK_DAYS = ['일', '월', '화', '수', '목', '금', '토'];
 
-class Moment {
-  constructor(current = new Date()) {
-    const { year, month, date, day } = convertDateToObject(current);
-    this.year = year;
-    this.month = month;
-    this.date = date;
-    this.day = day;
-  }
-
-  get weekDay() {
-    return WEEK_DAYS[this.day];
-  }
-}
-
 function convertDateToObject(date) {
   return {
     year: date.getFullYear(),
     month: date.getMonth() + 1,
     date: date.getDate(),
     day: date.getDay(),
+    time: date.getTime(),
   };
 }
 
+class Moment {
+  constructor(current = new Date()) {
+    const { year, month, date, day, time } = convertDateToObject(current);
+    this.year = year;
+    this.month = month;
+    this.date = date;
+    this.day = day;
+    this.time = time;
+  }
+
+  get weekDay() {
+    return WEEK_DAYS[this.day];
+  }
+
+  isToday() {
+    const today = new Moment(new Date());
+    return this.month === today.month && this.date === today.date;
+  }
+}
+
+export default Moment;
+
+/**
+ * @param {Date} date
+ * @return {Moment[]}
+ */
 export function getMonth(date) {
-  const dates = [];
+  date = new Date(date.getTime());
   date.setDate(1);
   date.setDate(1 - date.getDay());
 
+  const dates = [];
   for (let i = 0; i < 6; i++) {
     const week = [];
     for (let j = 0; j < 7; j++) {
