22459c2cbc9e4274fba87a9a7e0bf04c97e6d986
hoseol OH <dhgh9590@naver.com>
2022-06-30 15:46:48 +0900

    side bar

---
+++ front/src/components/SideNav/index.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from './style.module.css';
+
+const Index = () => {
+  return (
+    <div>
+      <aside className={styles.side_bar}>
+        <div className={styles.side_bar_icon}>
+          <ul>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+          </ul>
+        </div>
+      </aside>
+    </div>
+  );
+};
+
+export default Index;
