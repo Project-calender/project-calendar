8d8dc1c4b818b6186544464fe42652c810b4c32c
정진아 <human00024@naver.com>
2022-07-05 18:29:15 +0900

    feat: MonthCalendar 배치 조정, 오늘 날짜 표시, 주 단위 개수 계산 (#48)

--- front/src/components/calendar/month/CalendarBody/Date/index.jsx
+++ front/src/components/calendar/month/CalendarBody/Date/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './style.module.css';
+import Moment from '../../../../../utils/moment';
 
 const Index = ({ date }) => {
   return (
-    <td>
-      <em>{date.date}</em>
+    <td className={initClassName(date)}>
+      <em>{date.date === 1 ? `${date.month}월 1일` : date.date}</em>
     </td>
   );
 };
+
+function initClassName(date) {
+  let className = styles.calendar_td + ' ';
+  const today = new Moment(new Date());
+  if (isSameDate(date, today)) className += styles.calendar_today;
+
+  return className;
+}
+
+function isSameDate(date, otherDate) {
+  return (
+    date.year === otherDate.year &&
+    date.month === otherDate.month &&
+    date.date === otherDate.date
+  );
+}
 
 Index.propTypes = {
   date: PropTypes.object,
