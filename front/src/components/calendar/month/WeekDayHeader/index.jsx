0c6c6ab8b9d30d49fd3bfb9fe1f70e9c0cf4208a
정진아 <human00024@naver.com>
2022-07-05 19:04:33 +0900

    style: WeekDay 헤더 간격 조정, 오늘 날짜 표시 hover 추가 (#48)

--- front/src/components/calendar/month/WeekDayHeader/index.jsx
+++ front/src/components/calendar/month/WeekDayHeader/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { WEEK_DAYS } from '../../../../utils/moment';
+import styles from './style.module.css';
 
 const Index = () => {
   return (
-    <tr>
+    <tr className={styles.calendar_tr}>
       {WEEK_DAYS.map(item => {
         return <th key={item}>{item}</th>;
       })}
