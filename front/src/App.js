f900f72b5ad6800593f80695f8bc0bcad542f1aa
정진아 <human00024@naver.com>
2022-07-15 01:53:21 +0900

    feat: 기본 캘린더로 월 단위 캘린더 설정 (#86)

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './reset.css';
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 import Main from './pages/Main';
 import Login from './pages/Login';
@@ -9,28 +9,33 @@ import Join from './pages/Join';
 import YearCalendarPage from './pages/YearCalendarPage';
 import MonthCalendarPage from './pages/MonthCalendarPage';
 import DayCalendarPage from './pages/DayCalendarPage';
-import { CALENDAR_URL, USER_URL } from './constants/path';
+import { CALENDAR_PATH, USER_PATH } from './constants/path';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path={CALENDAR_URL.MAIN} element={<Main />}>
-          <Route path={CALENDAR_URL.DAY} element={<DayCalendarPage />}></Route>
-          <Route path={CALENDAR_URL.WEEK} element={<div>주</div>}></Route>
+        <Route path={CALENDAR_PATH.MAIN} element={<Main />}>
+          <Route index element={<Navigate to={CALENDAR_PATH.MONTH} />}></Route>
+          <Route path={CALENDAR_PATH.DAY} element={<DayCalendarPage />}></Route>
+          <Route path={CALENDAR_PATH.WEEK} element={<div>주</div>}></Route>
           <Route
-            path={CALENDAR_URL.MONTH}
+            path={CALENDAR_PATH.MONTH}
             element={<MonthCalendarPage />}
           ></Route>
           <Route
-            path={CALENDAR_URL.YEAR}
+            path={CALENDAR_PATH.YEAR}
             element={<YearCalendarPage />}
           ></Route>
-          <Route path={CALENDAR_URL.AGENDA} element={<div>일정</div>}></Route>
-          <Route path={CALENDAR_URL.CUSTOMDAY} element={<div>4일</div>}></Route>
+          <Route path={CALENDAR_PATH.AGENDA} element={<div>일정</div>}></Route>
+          <Route
+            path={CALENDAR_PATH.CUSTOMDAY}
+            element={<div>4일</div>}
+          ></Route>
         </Route>
-        <Route path={USER_URL.LOGIN} element={<Login />}></Route>
-        <Route path={USER_URL.JOIN} element={<Join />}></Route>
+        <Route path={USER_PATH.LOGIN} element={<Login />}></Route>
+        <Route path={USER_PATH.JOIN} element={<Join />}></Route>
+        <Route path="*" element={<Navigate to={CALENDAR_PATH.MONTH} />} />
       </Routes>
     </div>
   );
