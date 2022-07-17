9e984d63501d01b1d021d782fb80bf879518eb31
정진아 <human00024@naver.com>
2022-07-17 23:52:03 +0900

    refactor: 이벤트 바 동적 css - useRef 대신 dom의 style 속성 활용

--- front/src/components/calendar/month/CalendarBody/Date/EventBar/index.jsx
+++ front/src/components/calendar/month/CalendarBody/Date/EventBar/index.jsx
@@ -1,27 +1,22 @@
 import PropTypes from 'prop-types';
-import React, { useEffect, useRef } from 'react';
+import React from 'react';
 import { useSelector } from 'react-redux';
 import { calendarSelector } from '../../../../../../store/selectors/calendars';
 import styles from './style.module.css';
 
 const Index = ({ eventBar }) => {
-  const eventBarDiv = useRef();
   const calendar = useSelector(state =>
     calendarSelector(state, eventBar.PrivateCalendarId || eventBar.CalendarId),
   );
 
-  useEffect(() => {
-    if (calendar)
-      eventBarDiv.current.style.background = `linear-gradient(to right, ${calendar.color} 5px, ${eventBar.color} 5px)`;
-  }, [eventBarDiv.current]);
+  const eventBarDivStyle = {
+    background: `linear-gradient(to right, ${calendar?.color} 5px, ${eventBar.color} 5px)`,
+    width: `calc(100% * ${eventBar.scale} + ${eventBar.scale}px - 5px)`,
+  };
 
   if (!eventBar) return;
   return (
-    <div
-      className={styles.event_bar}
-      ref={eventBarDiv}
-      data-scale={eventBar.scale || 1}
-    >
+    <div className={styles.event_bar} style={eventBarDivStyle}>
       <em> {eventBar.name || '(제목 없음)'} </em>
     </div>
   );
