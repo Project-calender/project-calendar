4d70785dd2485ab99b3d6d64cc4bc825eae6e14e
정진아 <human00024@naver.com>
2022-07-18 14:28:37 +0900

    feat: events 상태 데이터 구조 변경 - 날짜별 events, eventBars 분류 (#102)

--- front/src/store/events.js
+++ front/src/store/events.js
@@ -1,10 +1,31 @@
 import { createEntityAdapter, createSlice } from '@reduxjs/toolkit';
+import Moment from '../utils/moment';
 import { fetchEvents } from './thunk';
 
+const eventSort = (event, other) => {
+  const eventDate = new Moment(new Date(event.startTime)).resetTime();
+  const otherDate = new Moment(new Date(other.startTime)).resetTime();
+
+  if (eventDate.time === otherDate.time) {
+    const eventPeriod = event.startTime - event.endTime;
+    const otherEventPeriod = other.startTime - other.endTime;
+    if (eventPeriod === otherEventPeriod)
+      return event.name.localeCompare(other.name);
+    if (eventPeriod > otherEventPeriod) return 1;
+    else return -1;
+  }
+
+  return eventDate.time - otherDate.time;
+};
+
 export const eventsAdapter = createEntityAdapter({
-  selectId: state => state.time,
+  selectId: state => state.id,
 });
-const initialState = eventsAdapter.getInitialState();
+
+const initialState = {
+  ...eventsAdapter.getInitialState(),
+  byDate: {},
+};
 
 const events = createSlice({
   name: 'events',
@@ -12,34 +33,43 @@ const events = createSlice({
   reducers: {},
   extraReducers: builder => {
     builder.addCase(fetchEvents.fulfilled, (state, { payload }) => {
-      const times = payload.events.reduce((times, event) => {
-        const startTime = new Date(event.startTime);
-        const key = new Date(new Date(startTime).toDateString()).getTime();
-
-        const time = times.get(key) || [];
-        time.push({
+      const events = payload.events
+        .map(event => ({
           ...event,
           startTime: new Date(event.startTime).getTime(),
           endTime: new Date(event.endTime).getTime(),
-        });
+        }))
+        .sort(eventSort);
 
-        return times.set(key, time);
-      }, new Map());
+      const byDate = events.reduce((byDate, event) => {
+        let startDate = new Moment(new Date(event.startTime)).resetTime();
+        const endDate = new Moment(new Date(event.endTime)).resetTime();
+        const key = startDate.time;
 
-      const data = [];
-      for (const [time, events] of times.entries()) {
-        events.sort(
-          (event, otherEvent) => event.startTime - otherEvent.startTime,
+        // 이벤트 추가
+        byDate[key] = byDate[key] || { events: [], eventBars: [] };
+        byDate[key].events.push(event.id);
+
+        // 이벤트 위치 배정
+        let index = byDate[key].eventBars.findIndex(event => !event);
+        index = index === -1 ? byDate[key].eventBars.length : index;
+        do {
+          const key = startDate.time;
+          byDate[key] = byDate[key] || { events: [], eventBars: [] };
+          byDate[key].eventBars[index] = event.id;
+        } while (
+          startDate.time !== endDate.time &&
+          (startDate = startDate.addDate(1))
         );
-        data.push({
-          time,
-          events,
-        });
-      }
-      eventsAdapter.setAll(state, data);
+
+        return byDate;
+      }, {});
+
+      eventsAdapter.setAll(state, events);
+      state.byDate = byDate;
     });
   },
 });
 
-export const { action } = events.actions;
+export const { setEventsByDate } = events.actions;
 export default events.reducer;
