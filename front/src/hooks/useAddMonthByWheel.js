71d19283235881bf55a71ca9f32123761fe132c7
정진아 <human00024@naver.com>
2022-07-19 18:35:33 +0900

    fix: Ctrl 누르면서 마우스 휠 이벤트 발생 시, 월 이동 방지

--- front/src/hooks/useAddMonthByWheel.js
+++ front/src/hooks/useAddMonthByWheel.js
@@ -5,6 +5,7 @@ export default function useAddMonthByWheel() {
   const dispatch = useDispatch();
 
   function changeMonth(e) {
+    if (e.nativeEvent.ctrlKey) return;
     if (e.deltaY > 0) dispatch(addMonth(1));
     else dispatch(addMonth(-1));
   }
