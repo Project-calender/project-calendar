586472e8a048565a1933f3b3c4dc789f0dff96c2
정진아 <human00024@naver.com>
2022-07-19 21:55:14 +0900

    style: 모달 이벤트바 스타일 지정 (#112)

--- front/src/components/calendar/month/CalendarBody/Date/EventBar/index.jsx
+++ front/src/components/calendar/month/CalendarBody/Date/EventBar/index.jsx
@@ -4,7 +4,12 @@ import { useSelector } from 'react-redux';
 import { calendarSelector } from '../../../../../../store/selectors/calendars';
 import styles from './style.module.css';
 
-const Index = ({ eventBar }) => {
+const Index = ({
+  eventBar,
+  left = false,
+  right = false,
+  outerRight = false,
+}) => {
   const calendar = useSelector(state =>
     calendarSelector(
       state,
@@ -13,23 +18,44 @@ const Index = ({ eventBar }) => {
     ),
   );
 
-  const scale = eventBar.scale || 1;
-  let eventBarDivStyle = {
-    background: `linear-gradient(to right, ${calendar?.color} 5px, ${eventBar.event?.color} 5px)`,
-    width: `calc(100% * ${scale} + ${scale}px - 5px)`,
+  const eventBarStyle = {
+    container: {
+      width: `calc(100% * ${eventBar?.scale} + ${eventBar?.scale}px - 5px)`,
+    },
+    main: {
+      background: `linear-gradient(to right, ${calendar?.color} 5px, ${eventBar.event?.color} 5px)`,
+    },
+    left: { borderRightColor: calendar?.color },
+    right: { borderLeftColor: eventBar.event?.color },
   };
 
-  if (!eventBar.scale) return <div className={`${styles.hidden}`}></div>;
-
   return (
-    <div className={styles.event_bar} style={eventBarDivStyle}>
-      <em> {eventBar.event?.name || '(제목 없음)'} </em>
+    <div className={styles.event_container} style={eventBarStyle.container}>
+      {left && <div className={styles.event_left} style={eventBarStyle.left} />}
+
+      {eventBar.scale && (
+        <div className={styles.event_bar} style={eventBarStyle.main}>
+          <em> {eventBar.event?.name || '(제목 없음)'} </em>
+        </div>
+      )}
+
+      {right && (
+        <div
+          className={`${styles.event_right} ${
+            outerRight ? styles.event_right_outer : null
+          }`}
+          style={eventBarStyle.right}
+        />
+      )}
     </div>
   );
 };
 
 Index.propTypes = {
   eventBar: PropTypes.object,
+  left: PropTypes.bool,
+  right: PropTypes.bool,
+  outerRight: PropTypes.bool,
 };
 
 export default Index;
--- front/src/components/calendar/month/EventListModal/index.jsx
+++ front/src/components/calendar/month/EventListModal/index.jsx
@@ -3,19 +3,31 @@ import styles from './style.module.css';
 import PropTypes from 'prop-types';
 import Modal from '../../../common/Modal';
 import EventBar from '../CalendarBody/Date/EventBar';
+import Moment from '../../../../utils/moment';
 
 export const triggerDOM = 'month-event-list';
 
 const Index = ({ modalData, hideModal }) => {
   const { date, events, position } = modalData;
-
   return (
     <Modal hideModal={hideModal} triggerDOM={triggerDOM} position={position}>
       <div className={styles.modal_container}>
         <strong>{date.weekDay}</strong>
         <h1>{date.date}</h1>
-        {events.map(info => (
-          <EventBar key={info.event.id} eventBar={info} />
+        {events.map(eventInfo => (
+          <EventBar
+            key={eventInfo.event.id}
+            eventBar={eventInfo}
+            left={
+              new Moment(new Date(eventInfo.event.startTime)).resetTime()
+                .time === date.time
+            }
+            right={
+              new Moment(new Date(eventInfo.event.endTime)).resetTime().time ===
+              date.time
+            }
+            outerRight={true}
+          />
         ))}
       </div>
     </Modal>
