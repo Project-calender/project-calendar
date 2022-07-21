599cc0acd10b6d6143f758af0f0a1119ea2b7b44
이민서 <sola2014@naver.com>
2022-07-21 18:22:34 +0900

    fix: 토큰 에러

--- back/utils/authJWT.js
+++ back/utils/authJWT.js
@@ -3,7 +3,12 @@ const { verify } = require("./jwt-util");
 const authJWT = (req, res, next) => {
   req.headers.authorization = req.headers.authorization.substr(1);
   req.headers.authorization = req.headers.authorization.slice(0, -1);
-
+  if (req.headers.authorization == undefined || req.headers.authorizatio == null) {
+    return res.status(401).send({
+      ok:false,
+      message: "accessToken이 지급되지 않았습니다"
+    })
+  }
   if (req.headers.authorization) {
     const token = req.headers.authorization; // header에서 access token을 가져옵니다.
     const result = verify(token); // token을 검증합니다.
@@ -13,18 +18,17 @@ const authJWT = (req, res, next) => {
       next();
     } else {
       // 검증에 실패하거나 토큰이 만료되었다면 클라이언트에게 메세지를 담아서 응답합니다.
-      res.status(401).send({
+      res.status(402).send({
         ok: false,
         message: result.message, // jwt가 만료되었다면 메세지는 'jwt expired'입니다.
       });
     }
   } else {
     //세션 스토리지에 access 토큰이 없는 경우
-    res.status(401).send({
+    return res.status(401).send({
       ok:false,
       message: "accessToken이 지급되지 않았습니다"
     })
-    
   }
 };
 //dasdas
