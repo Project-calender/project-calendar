ffa1e89a8a8398548905389d1718a649a690bc02
Minseo Lee <73999410+nacho1415@users.noreply.github.com>
2022-08-06 20:02:46 +0900

    Fix (#202)
    * refator: 응답 객체명 변경
    
    * fix: error

--- back/routes/privateCalendar.js
+++ back/routes/privateCalendar.js
@@ -12,11 +12,11 @@ router.post('/editPrivateCalendar', authJWT, async (req, res, next) => {
             }
         })
 
-        if (!exPrivateCalendar) {
+        if (!changeCalendar) {
             return res.status(400).send({ message: "개인 캘린더 조회 결과가 없습니다 입력값을 다시 확인해주세요" })
         } 
         // console.log(exPrivateCalendar.UserId)
-        if (req.myId != exPrivateCalendar.UserId) {
+        if (req.myId != changeCalendar.UserId) {
             return res.status(401).send({ message: "수정 시도하는 유저가 개인 캘린더의 주인이 아닙니다"})
         }
 
