d908b91c70af834c40122d2cb3abdb4289c091fe
정진아 <human00024@naver.com>
2022-08-26 15:13:34 +0900

    [feat] 마우스 드래그로 이벤트 생성 모달 이동 (#257)

--- front/src/components/common/Modal/index.jsx
+++ front/src/components/common/Modal/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +6,9 @@ import PropTypes from 'prop-types';
 import Tooltip from '../Tooltip';
 import { useEffect } from 'react';
 import { useRef } from 'react';
+import { throttle } from '../../../utils/delay';
+import { useCallback } from 'react';
+import { useMemo } from 'react';
 
 const Index = ({
   children,
@@ -15,7 +18,23 @@ const Index = ({
   isBackground = false,
   backgroundColor = 'transform',
 }) => {
-  const $modal = useRef();
+  const modalRef = useRef();
+  const modalBackgroundRef = useRef();
+
+  const [isMouseDown, setMouseDown] = useState(false);
+  const pos = useMemo(() => ({ x: 0, y: 0 }), []);
+  const moveModal = useCallback(e => {
+    if (e.pageY === pos.y && e.pageX === pos.x) return;
+    throttle(() => {
+      const $modal = modalRef.current;
+      $modal.style.top = $modal.offsetTop + (e.pageY - pos.y) + 'px';
+      $modal.style.left = $modal.offsetLeft + (e.pageX - pos.x) + 'px';
+
+      pos.x = e.pageX;
+      pos.y = e.pageY;
+    }, 100);
+  }, []);
+
   useEffect(() => {
     document.addEventListener('click', clickModalOutside);
     document.body.style.overflow = 'hidden';
@@ -26,7 +45,7 @@ const Index = ({
   });
 
   function clickModalOutside(event) {
-    if (!$modal.current.contains(event.target)) {
+    if (!isMouseDown && !modalRef.current.contains(event.target)) {
       hideModal();
     }
   }
@@ -35,16 +54,41 @@ const Index = ({
     <>
       {isBackground && (
         <div
-          className={styles.modal_background}
+          className={`${styles.modal_background} ${
+            isMouseDown ? styles.max_zIndex : ''
+          }`}
           style={{ backgroundColor }}
           onWheel={e => e.stopPropagation()}
+          ref={modalBackgroundRef}
+          onMouseUp={() => {
+            modalBackgroundRef.current.removeEventListener(
+              'mousemove',
+              moveModal,
+            );
+            if (isMouseDown)
+              setTimeout(() => {
+                setMouseDown(false);
+              }, 100);
+          }}
         />
       )}
       <div
         className={styles.modal_container}
-        ref={$modal}
+        ref={modalRef}
+        name="modal"
         style={style}
         onWheel={e => e.stopPropagation()}
+        onMouseDown={e => {
+          if (e.target.getAttribute('name') === 'modal-move-trigger') {
+            modalBackgroundRef.current.addEventListener('mousemove', moveModal);
+            pos.x = e.pageX;
+            pos.y = e.pageY;
+            setMouseDown(true);
+          }
+        }}
+        onMouseUp={() => {
+          if (isMouseDown) setMouseDown(false);
+        }}
       >
         {isCloseButtom && (
           <div className={styles.modal_close_icon}>
