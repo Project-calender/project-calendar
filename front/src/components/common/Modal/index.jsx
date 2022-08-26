310d72c064dc11751393d0857f7b7203a416aaba
정진아 <human00024@naver.com>
2022-08-26 16:07:41 +0900

    [feat] useDraggable Hook 구현 (#257)

--- front/src/components/common/Modal/index.jsx
+++ front/src/components/common/Modal/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import styles from './style.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
@@ -6,9 +6,7 @@ import PropTypes from 'prop-types';
 import Tooltip from '../Tooltip';
 import { useEffect } from 'react';
 import { useRef } from 'react';
-import { throttle } from '../../../utils/delay';
-import { useCallback } from 'react';
-import { useMemo } from 'react';
+import useDraggable from '../../../hooks/useDraggable';
 
 const Index = ({
   children,
@@ -21,19 +19,10 @@ const Index = ({
   const modalRef = useRef();
   const modalBackgroundRef = useRef();
 
-  const [isMouseDown, setMouseDown] = useState(false);
-  const pos = useMemo(() => ({ x: 0, y: 0 }), []);
-  const moveModal = useCallback(e => {
-    if (e.pageY === pos.y && e.pageX === pos.x) return;
-    throttle(() => {
-      const $modal = modalRef.current;
-      $modal.style.top = $modal.offsetTop + (e.pageY - pos.y) + 'px';
-      $modal.style.left = $modal.offsetLeft + (e.pageX - pos.x) + 'px';
-
-      pos.x = e.pageX;
-      pos.y = e.pageY;
-    }, 100);
-  }, []);
+  const { isMouseDown, readyMoveElement, stopMoveElement } = useDraggable(
+    modalRef,
+    modalBackgroundRef,
+  );
 
   useEffect(() => {
     document.addEventListener('click', clickModalOutside);
@@ -60,16 +49,7 @@ const Index = ({
           style={{ backgroundColor }}
           onWheel={e => e.stopPropagation()}
           ref={modalBackgroundRef}
-          onMouseUp={() => {
-            modalBackgroundRef.current.removeEventListener(
-              'mousemove',
-              moveModal,
-            );
-            if (isMouseDown)
-              setTimeout(() => {
-                setMouseDown(false);
-              }, 100);
-          }}
+          onMouseUp={stopMoveElement}
         />
       )}
       <div
@@ -79,15 +59,8 @@ const Index = ({
         style={style}
         onWheel={e => e.stopPropagation()}
         onMouseDown={e => {
-          if (e.target.getAttribute('name') === 'modal-move-trigger') {
-            modalBackgroundRef.current.addEventListener('mousemove', moveModal);
-            pos.x = e.pageX;
-            pos.y = e.pageY;
-            setMouseDown(true);
-          }
-        }}
-        onMouseUp={() => {
-          if (isMouseDown) setMouseDown(false);
+          if (e.target.getAttribute('name') !== 'modal-move-trigger') return;
+          readyMoveElement(e);
         }}
       >
         {isCloseButtom && (
