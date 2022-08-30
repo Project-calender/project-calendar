9925d224252d35696246af7b8388f31ec9ba748f
dhgh9590 <79558348+dhgh9590@users.noreply.github.com>
2022-08-30 22:00:12 +0900

    Update index.jsx

--- front/src/components/searchDetall/index.jsx
+++ front/src/components/searchDetall/index.jsx
@@ -38,14 +38,6 @@ const Index = () => {
     setSearchFilter(event);
   }, [searchValue]);
 
-  //날짜 순으로 정렬
-  searchValue = copySearchValue.sort(function (a, b) {
-    return (
-      a.startTime.substr(0, 10).replaceAll('-', '') -
-      b.startTime.substr(0, 10).replaceAll('-', '')
-    );
-  });
-
 
   //이벤트 팝업창 컨트롤
   async function clickEventBar(e, event) {
