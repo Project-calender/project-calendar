fdb61acd8a52137de723a76ac8c92304b866a615
hoseol OH <dhgh9590@naver.com>
2022-08-30 19:45:34 +0900

    일정페이지 ,검색 페이지 수정

--- front/src/pages/AllEvent/index.jsx
+++ front/src/pages/AllEvent/index.jsx
@@ -20,7 +20,6 @@ const Index = () => {
   let [nextDay, setNextDay] = useState(); //다음 해 날짜
   let [allEvent, setAllEvent] = useState(); //모든 이벤트
   let [filterEvent, setFilterEvent] = useState(); //필터된 이벤트
-  let [day, setDay] = useState(); //한국 일자
   let [clickEvent, setClickEvent] = useState(); //클릭한 이벤트 index
   let eventItem = useRef(); //이벤트 아이템
 
@@ -76,17 +75,14 @@ const Index = () => {
         })
         .filter(isCheckedCalander); //캘린더에 체크된 이벤트만 필터
 
-    //한국 날짜로 변경
-    let newToDay =
-      newAllEvent &&
-      newAllEvent.map(a => {
-        let date = new Date(a.startTime);
-        const day = ('0' + date.getDate()).slice(-2);
-        return day;
-      });
+    let event = newAllEvent?.reduce((obj, event) => {
+      obj[new Date(event.startTime).getTime()] =
+        obj[new Date(event.startTime).getTime()] || [];
+      obj[new Date(event.startTime).getTime()].push(event);
+      return obj;
+    }, {});
 
-    setDay(newToDay); //한국 날짜 저장
-    setFilterEvent(newAllEvent); //필터된 이벤트 배열로 저장
+    setFilterEvent(event); //필터된 이벤트 배열로 저장
   }
 
   //이벤트 팝업창 컨트롤
@@ -124,56 +120,68 @@ const Index = () => {
         <EventDetailModal hideModal={hideModal} modalData={modalData} />
       )}
       <div className={styles.container}>
-        <ul>
-          {filterEvent &&
-            filterEvent.map(function (item, index) {
+        {filterEvent && (
+          <ul>
+            {Object.entries(filterEvent).map(function ([time, item], index) {
               return (
                 <li key={index}>
                   <div className={styles.time}>
                     <div className={styles.day}>
-                      <em>{day[index]}</em>
+                      <em>{new Date(+time).getDate()}</em>
                     </div>
                     <div className={styles.year}>
                       <p>
-                        {item.startTime.substr(0, 4)}년
-                        {item.startTime.substr(5, 2)}월
+                        {new Date(+time).getFullYear()}년
+                        {new Date(+time).getMonth() + 1}월
                       </p>
                     </div>
                   </div>
-                  <div
-                    ref={eventItem}
-                    className={
-                      clickEvent == index
-                        ? `${styles.active} ${styles.event_wrap}`
-                        : styles.event_wrap
-                    }
-                    onClick={e => {
-                      clickEventBar(e, item);
-                      setClickEvent(index);
-                    }}
-                  >
-                    <div className={styles.all_day}>
-                      {item.allDay == EVENT.allDay.true ||
-                      item.endTime.substr(5, 5).replace('-', '') -
-                        item.startTime.substr(5, 5).replace('-', '') >
-                        1 ? (
-                        <em>종일</em>
-                      ) : (
-                        <em>
-                          {item.startTime.substr(11, 5)} ~{' '}
-                          {item.endTime.substr(11, 5)}
-                        </em>
-                      )}
-                    </div>
-                    <div className={styles.content}>
-                      <p>{item.name}</p>
-                      {item.name.length == 0 ? <p>(제목 없음)</p> : null}
-                    </div>
+                  <div className={styles.wrap}>
+                    {item.map((item, index) => {
+                      return (
+                        <div
+                          key={index}
+                          ref={eventItem}
+                          className={
+                            clickEvent == item.id
+                              ? `${styles.active} ${styles.event_wrap}`
+                              : styles.event_wrap
+                          }
+                          onClick={e => {
+                            clickEventBar(e, item);
+                            setClickEvent(item.id);
+                          }}
+                        >
+                          <div className={styles.all_day}>
+                            {item.allDay == EVENT.allDay.true ||
+                            item.endTime.substr(5, 5).replace('-', '') -
+                              item.startTime.substr(5, 5).replace('-', '') >
+                              1 ? (
+                              <div>
+                                <span style={{ background: item.color }}></span>
+                                <em>종일</em>
+                              </div>
+                            ) : (
+                              <em>
+                                {item.startTime.substr(11, 5)} ~{' '}
+                                {item.endTime.substr(11, 5)}
+                              </em>
+                            )}
+                          </div>
+                          <div className={styles.content}>
+                            <p>{item.name}</p>
+                            {item.name.length == 0 ? <p>(제목 없음)</p> : null}
+                          </div>
+                        </div>
+                      );
+                    })}
                   </div>
                 </li>
               );
             })}
-        </ul>
+          </ul>
+        )}
+
         {filterEvent && filterEvent.length == 0 ? (
           <div className={styles.search_undefined}>
             <em>이벤트 결과가 없습니다.</em>
