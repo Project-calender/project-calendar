e5d5672ab4a4827b8bdce008413712495c75fcf3
정진아 <human00024@naver.com>
2022-09-21 17:42:32 +0900

    feat: 날짜 클릭 시, day 캘린더 이동

--- front/src/components/calendar/TimeCalendar/CalendarHeader/index.jsx
+++ front/src/components/calendar/TimeCalendar/CalendarHeader/index.jsx
@@ -3,16 +3,23 @@ import PropTypes from 'prop-types';
 import styles from './style.module.css';
 
 import Moment from '../../../../utils/moment';
-
+import useNavigateDayCalendar from '../../../../hooks/useNavigateDayCalendar';
 const Index = ({ dates = [] }) => {
   const isToday = date => date.time === new Moment().time;
-
+  const { moveDayCalendar } = useNavigateDayCalendar();
   return (
     <div className={styles.calendar_header}>
       {dates.map(date => (
         <div key={date.time} className={`${isToday(date) ? styles.today : ''}`}>
           <em>{date.weekDay}</em>
-          <p>{date.date}</p>
+          <p
+            className={styles.calendar_header_date}
+            onClick={() => {
+              moveDayCalendar(date);
+            }}
+          >
+            {date.date}
+          </p>
         </div>
       ))}
     </div>
