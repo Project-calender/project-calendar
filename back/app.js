03deaf01b97f5157af7e76a221cc0eb43a759d30
minwoo <gkqkehs7@github.com>
2022-11-10 15:33:26 +0900

    server build complete

--- back/app.js
+++ back/app.js
@@ -44,6 +44,13 @@ app.use(
     credentials: true,
   })
 );
+
+app.all("/*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
+
 app.use(helmet({ contentSecurityPolicy: false }));
 app.use(hpp());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -53,6 +60,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(passport.initialize());
 
+app.use(express.static(path.join(__dirname, "public")));
 //라우터
 app.use(
   "/api-docs",
@@ -69,10 +77,9 @@ app.use(function (error, req, res, next) {
   res.json({ message: error.message });
 });
 
-app.get("/", (req, res) => {
-  res.send("hi");
+app.get("*", (req, res, next) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 useSocket(httpServer, app);
 
 //포트 설정
